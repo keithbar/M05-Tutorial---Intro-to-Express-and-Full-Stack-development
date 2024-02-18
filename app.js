1a59222b56fb57beef464900ea8f4f6a295b3709
Keith <kbarber34@ivytech.edu>
2024-02-18 07:51:38 -0500

    replace html with ejs views

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-    res.sendFile("./views/index.html", {root: __dirname});
+    res.render("index");
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("./views/about.html", {root: __dirname});
+    res.render("about");
 });
 
 //redirects
@@ -24,5 +24,5 @@ app.get("/about-us", (req, res) => {
 
 //404
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", {root: __dirname});
+    res.status(404).render("404");
 });
